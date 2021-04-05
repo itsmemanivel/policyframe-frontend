c2759d94bfeec56bfce2e76cc8e1a8adb164ae64
veluvj <veluvijay1804@gmail.com>
2021-04-05 10:05:59 +0530

    firebase prod

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -6,8 +6,8 @@ import { ApolloModule, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const uri = 'http://localhost:4000/graphql'; // Development
-
+// const uri = 'http://localhost:4000/graphql'; // Development
+const uri = "https://policyframe-api.herokuapp.com/graphql" //Production
 
 @NgModule({
   exports: [
